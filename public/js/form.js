7b869e8691849315d544b6b1075c7da6ac66f146
aman-kumar-jh <15uec012@lnmiit.ac.in>
2019-11-18 16:08:08 +0530

    redirecting to signIn page

--- public/js/form.js
+++ public/js/form.js
@@ -1,24 +1,15 @@
 //var userData = require('./main.js.js');
 
-console.log(userData);
+//console.log(userData);
 
 var details = document.getElementById("myform");
 let register = document.getElementById("regs");
 
-register.addEventListener("click", registerUser);
-
-var registerUser = function() {
+register.addEventListener("click", e => {
   e.preventDefault();
   let data = {
-    "first name": firstName.value,
-    "last name": lastName.value,
-    college: collegeName.value,
-    address: address1.value + address2.value,
-    zip: zipCode.value,
-    state: state.value,
-    country: countryCode.value,
-    "country code": countryCode.value,
-    "phone number": phoneNumber.value
+    userDetail: register.value
   };
   console.log(data);
-};
+  window.location.href = "html/sign_in.html";
+});
